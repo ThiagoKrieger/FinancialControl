d2ea5d761c5df503b1aa4b5bb6c9fb7d57863715
Thiago Krieger <thiago.krieger@cppro.com.br>
2022-12-01 22:33:12 -0300

    InitialCommit- Adds the sample project and the packages

---
+++ WebApplication1/Models/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models;
+
+public class UserViewModel
+{
+    public int Id { get; set; }
+    public int Age { get; set; }
+    public string Name { get; set; }
+    public float Balance { get; set; }
+    public ICollection<TransactionViewModel> TransactionViewModels { get; set; }
+}

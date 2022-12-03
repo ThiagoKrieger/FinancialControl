80922abc81e901c11c0e7145128cfa029e476323
Thiago Krieger <thiago.krieger@cppro.com.br>
2022-12-03 17:35:51 -0300

    BackEnd-Adds Utils to the project in order to project to a data provider

---
+++ Utils/ObjectExtension.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+namespace Utils;
+
+public static class ObjectExtension
+{
+    public static TDestination ProjectToNew<TDestination>(this object source)
+        where TDestination : class, new()
+    {
+        var target = new TDestination();
+        source.ProjectTo(target);
+        return target;
+    }
+
+    public static void ProjectTo(this object? source, object target)
+    {
+        if (source == null)
+            return;
+
+        if (target == null)
+            throw new ArgumentNullException(nameof(target));
+
+        var sourceProperties = ReflectionUtils.GetPocoProperties(source.GetType());
+        var targetProperties = ReflectionUtils.GetPocoProperties(target.GetType());
+
+        foreach (var (name, sourceProperty) in sourceProperties)
+        {
+            if (!targetProperties.TryGetValue(name, out var targetProperty))
+                continue;
+
+            if (targetProperty.IsDefined(typeof(NotMappedAttribute)))
+                continue;
+
+            if (!targetProperty.PropertyType.IsValueType && targetProperty.PropertyType != typeof(string))
+                continue;
+
+            var sourceValue = sourceProperty.GetValue(source, null);
+            var targetValue = targetProperty.GetValue(target, null);
+
+            if (sourceValue != targetValue)
+                targetProperty.SetValue(target, sourceValue, null);
+        }
+    }
+}
